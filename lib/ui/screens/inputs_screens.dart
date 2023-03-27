72d6692f01bd6855660b9dcebe6da4623444e348
arlettearenas <arlettearenas29@gmail.com>
2023-03-27 14:02:55 -0600

    Agregando campos de usuario y password

--- lib/ui/screens/inputs_screens.dart
+++ lib/ui/screens/inputs_screens.dart
@@ -13,24 +13,71 @@ class _InputsScreensState extends State<InputsScreens> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.purple,
+        backgroundColor: Color.fromARGB(255, 94, 7, 109),
         title: const Text('Entradas de datos por el usuario'),
       ),
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
         children: [
-          _crearEntrada(),
+          _crearEntradaNombre(),
+          _crearEntradaPswd(),
         ],
       ),
     );
   }
 
-  Widget _crearEntrada() {
+  Widget _crearEntradaNombre() {
     return TextField(
         autofocus: true,
-        textCapitalization: TextCapitalization.words,
+        textAlign: TextAlign.center,
+        textCapitalization: TextCapitalization.sentences,
+        style: const TextStyle(
+          color: Color.fromARGB(255, 83, 80, 184),
+          fontWeight: FontWeight.bold,
+        ),
+        cursorColor: Color.fromARGB(255, 244, 175, 198),
+        cursorRadius: const Radius.circular(5.0),
+        cursorWidth: 5.0,
+        //maxLength: 20,
+        maxLines: null,
         decoration: InputDecoration(
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+          hintText: "Escribe tu nombre de ususario mi amorcito",
+          labelText: "Nombre: ",
+          //helperText: "Nombre",
+          prefixIcon: const Icon(Icons.person),
+          //iconColor: Color.fromARGB(255, 108, 106, 111),
+          //prefix: const CircularProgressIndicator(),
+        ),
+        onChanged: (valor) {
+          _userName = valor;
+          print(_userName);
+        });
+  }
+
+  Widget _crearEntradaPswd() {
+    return TextField(
+        autofocus: true,
+        textAlign: TextAlign.center,
+        textCapitalization: TextCapitalization.sentences,
+        style: const TextStyle(
+          color: Color.fromARGB(255, 83, 80, 184),
+          fontWeight: FontWeight.bold,
+        ),
+        cursorColor: const Color.fromARGB(255, 244, 175, 198),
+        cursorRadius: const Radius.circular(5.0),
+        cursorWidth: 5.0,
+        maxLength: 8,
+        obscureText: true,
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+          hintText: "Escribe tu password",
+          labelText: "Password: ",
+          //helperText: "Nombre",
+          suffixIcon: const Icon(Icons.key),
+          //iconColor: Color.fromARGB(255, 108, 106, 111),
+          //prefix: const CircularProgressIndicator(),
         ),
         onChanged: (valor) {
           _userName = valor;
